e7307129363a17874a6b32c36a1013acb9de58bc
xie-james72 <44209727+xie-james72@users.noreply.github.com>
2019-10-25 18:27:33 -0600

    Removed unused methods and EVI band

--- MODIS_API/RatioCalculation.py
+++ MODIS_API/RatioCalculation.py
@@ -1,57 +1,4 @@
-import os
 import numpy as np
-from pathlib import Path
-import matplotlib.pyplot as plt
-
-ROOT_DIR = os.path.dirname(
-    os.path.abspath(__file__))  # This is your Project Root
-IMG_DIR = Path("../pictures/test/")  # relative path from ROOT_DIR
-FILE_EXT = ".tiff"
-EXT_LENGTH = 5
-
-
-def show_image(img):
-    plt.imshow(img)
-    plt.show()
-
-
-def calculate_ratios(file_name_1, file_name_2):
-    file_1 = generate_file_path(file_name_1)
-    file_2 = generate_file_path(file_name_2)
-
-    img_arr1 = plt.imread(file_1)
-    img_arr2 = plt.imread(file_2)
-
-    save_file = generate_save_name(file_name_1, file_name_2)
-
-    ndvi_calculation(img_arr1, img_arr2, save_file)
-
-
-def generate_file_path(file):
-    """ Returns the path to the given file """
-    path = ROOT_DIR / IMG_DIR / (file + ".tiff")
-    return path
-
-
-def generate_save_name(file1, file2):
-    """ Returns a name for the save file of the calculation results """
-    save_file = file1 + '_' + file2
-    return save_file
-
-
-def ndvi_calculation(arr1, arr2, save_file):
-    """ 
-        Calculates the R/G ratios using the NDVI formula:
-        NDVI = (Band 1 - Band 5) / (Band 1 + Band 5)
-    """
-
-    sub_arr = np.subtract(arr1, arr2)
-    add_arr = np.add(arr1, arr2)
-    div_arr = np.divide(sub_arr, add_arr)
-
-    np.save(save_file, div_arr)
-    # load_print_file("img1-img2")
-
 
 def ndvi_calculation(arr1, arr2):
     """
@@ -84,23 +31,67 @@ def evi_calculation(blue, red, infrared):
 
 def add_ratio_bands(rgb):
     ndvi_band = ndvi_calculation(rgb[1], rgb[0])  # pass in red, IR
-    evi_band = evi_calculation(rgb[2], rgb[1], rgb[0])  # pass in blue, red, IR
+    #evi_band = evi_calculation(rgb[2], rgb[1], rgb[0])  # pass in blue, red, IR
     rgb.append(ndvi_band)
-    rgb.append(evi_band)
+    #rgb.append(evi_band)
 
     # return a numpy array instead of a list
     return np.array(rgb)
 
+### Function call ###
+# calculate_ratios("img1", "img2")
+"""
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
 
+ROOT_DIR = os.path.dirname(
+    os.path.abspath(__file__))  # This is your Project Root
+IMG_DIR = Path("../pictures/test/")  # relative path from ROOT_DIR
+FILE_EXT = ".tiff"
+EXT_LENGTH = 5
+
+def show_image(img):
+    plt.imshow(img)
+    plt.show()
+    
 def load_print_file(file):
-    """ 
-        loads and prints a given .npy file 
-        (do not add an extension to file name) 
-    """
+    #loads and prints a given .npy file 
+    #(do not add an extension to file name)
 
     infile = np.load(file + ".npy")
     print(file + ' ')
     print(infile)
+    
+def ndvi_calculation(arr1, arr2, save_file):
+    #Calculates the R/G ratios using the NDVI formula:
+    #NDVI = (Band 1 - Band 5) / (Band 1 + Band 5)
+    
+    sub_arr = np.subtract(arr1, arr2)
+    add_arr = np.add(arr1, arr2)
+    div_arr = np.divide(sub_arr, add_arr)
 
-### Function call ###
-# calculate_ratios("img1", "img2")
+    np.save(save_file, div_arr)
+    # load_print_file("img1-img2")
+
+def calculate_ratios(file_name_1, file_name_2):
+    file_1 = generate_file_path(file_name_1)
+    file_2 = generate_file_path(file_name_2)
+
+    img_arr1 = plt.imread(file_1)
+    img_arr2 = plt.imread(file_2)
+
+    save_file = generate_save_name(file_name_1, file_name_2)
+
+    ndvi_calculation(img_arr1, img_arr2, save_file)
+
+def generate_file_path(file):
+    #Returns the path to the given file
+    path = ROOT_DIR / IMG_DIR / (file + ".tiff")
+    return path
+
+def generate_save_name(file1, file2):
+    #Returns a name for the save file of the calculation results
+    save_file = file1 + '_' + file2
+    return save_file
+"""
