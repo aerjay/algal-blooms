a5e5607b5e0ba52e9c324743ca11d89a96a0899f
nguc <c.ngu.91@gmail.com>
2019-10-26 12:45:07 -0600

    Adding an overload method
    Adding an overloaded method of generate_json so that you can pass in an array of ratios and dates that correspond to each other

--- MODIS_API/graphCSV.py
+++ MODIS_API/graphCSV.py
@@ -6,43 +6,45 @@
 #the array with the output data. That array may then be exported as a csv for the front end
 import numpy as np
 
+
 def graphData(img, labels, water_label):
     #receive the image containing (IR, red, green) and NDVI data in the shape 4 x 2400 x 2400
     #as well as the KMeans labels (as 1D array) and an indicator for which label is for water
-    
-    NDVI_layer = np.zeros((2400,2400), dtype = float)
-    red_layer = np.zeros((2400,2400), dtype = float)
-    
+
+    NDVI_layer = np.zeros((2400, 2400), dtype=float)
+    red_layer = np.zeros((2400, 2400), dtype=float)
+
     #Make all the labels 1 if water, else 0. Then reshape it to the 2400x2400 image
     for i, label in enumerate(labels):
         labels[i] = (label == water_label)
-        
-    labels = labels.reshape(2400,2400)
-    
+
+    labels = labels.reshape(2400, 2400)
+
     #separate out the NDVI and red bands of interest from the original image, omitting non-water pixels
-    NDVI_layer = np.multiply(img[3,:,:], labels)
-    red_layer = np.multiply(img[1,:,:], labels)
-    
+    NDVI_layer = np.multiply(img[3, :, :], labels)
+    red_layer = np.multiply(img[1, :, :], labels)
+
     #calculate the mean, 90th percentile, and 10th percentile of NDVI and chlorophyll
     NDVI_avg = np.mean(NDVI_layer)
     Chl_avg = np.mean(red_layer)
-    
-    NDVI_lower, NDVI_upper = np.percentile(NDVI_layer, [10,90])
-    Chl_lower, Chl_upper = np.percentile(red_layer, [10,90])
-    
-    graph_data = [NDVI_avg, NDVI_upper, NDVI_lower, Chl_avg, Chl_upper, Chl_lower]
-    
+
+    NDVI_lower, NDVI_upper = np.percentile(NDVI_layer, [10, 90])
+    Chl_lower, Chl_upper = np.percentile(red_layer, [10, 90])
+
+    graph_data = [
+        NDVI_avg, NDVI_upper, NDVI_lower, Chl_avg, Chl_upper, Chl_lower
+    ]
+
     return graph_data
 
 
+# #to be included in main:
+#     #file name will include lat, long, and end date of the images
+# graphCSV = np.zeros(6, dtype = float)
 
-#to be included in main:
-    #file name will include lat, long, and end date of the images
-graphCSV = np.zeros(6, dtype = float)
+# #integrate this with whatever loop generates each of the 6 images
+# for j in range(6):
+#     #... Chi & Ben's code before
+#     graphCSV[j] = graphData(rgb, labels, water)
 
-#integrate this with whatever loop generates each of the 6 images
-for j in range(6):
-    #... Chi & Ben's code before
-    graphCSV[j] = graphData(rgb, labels, water)
-
-np.savetxt(lat+"-"+long+"-"+date+".csv", graphCSV, delimiter = ",")
+# np.savetxt(lat+"-"+long+"-"+date+".csv", graphCSV, delimiter = ",")
--- generate_json.py
+++ generate_json.py
@@ -8,6 +8,7 @@ import numpy as np
 import json
 
 
+# ratio_band is a single array of ratios
 def generate_json(ratio_band):
     #convert arrays to list so json can serialize
     ir = ratio_band[0].tolist()
@@ -20,31 +21,48 @@ def generate_json(ratio_band):
         json.dump(data, outfile)
 
 
+# ratio_bands is an array of ratios arrays
+# dates is an array of dates corresponding to the ratio array
+def generate_json(ratio_bands, dates):
+    #convert arrays to list so json can serialize
+    ir = ratio_bands[0].tolist()
+    red = ratio_bands[1].tolist()
+    green = ratio_bands[2].tolist()
+
+    data = {}
+    data[dates[0]] = []
+    data[dates[0]].append({"IR": ir, "Red": red, "Green": green})
+
+    with open('ratios_dates.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
 ### Code to example run ###
 
-# Let's look at the tile containing Chicago, IL, on May 15, 2019 (day of year 135)
+#Let's look at the tile containing Chicago, IL, on May 15, 2019 (day of year 135)
+product = 'MCD43A4'  # Surface reflectance
+dayNum = '2011252'
+lat = 42.124753
+lon = -81.769110
+date = ["05/15/2019"]
+
+# for gotland algae bloom
 # product = 'MCD43A4'  # Surface reflectance
-# dayNum = '2011252'
-# lat = 42.124753
-# lon = -81.769110
+# dayNum = '2005194'
+# lat = 57.4684
+# lon = 18.4867
+remove_files_on_finish = False
 
-# # for gotland algae bloom
-# # product = 'MCD43A4'  # Surface reflectance
-# # dayNum = '2005194'
-# # lat = 57.4684
-# # lon = 18.4867
-# remove_files_on_finish = False
+# uncomment below line if you need to re-query images from the API
+# MODIS_Module.download_data_to_folder(product, lat, lon, dayNum, remove_files_on_finish)
 
-# # uncomment below line if you need to re-query images from the API
-# # MODIS_Module.download_data_to_folder(product, lat, lon, dayNum, remove_files_on_finish)
+rgb = MODIS_Module.get_modis_bands_array()  # 3 x 2400 x 2400
 
-# rgb = MODIS_Module.get_modis_bands_array()  # 3 x 2400 x 2400
+# This just combines the original .tiff files into a false-colour image
+# MODIS_Module.show_modis_image()
 
-# # This just combines the original .tiff files into a false-colour image
-# # MODIS_Module.show_modis_image()
+# chi's code - calculate ratios(rgb) -> output 5 x 2400 x 2400
+rgb = RatioCalculation.add_ratio_bands(rgb)
+band_ratios = rgb * 25
 
-# # chi's code - calculate ratios(rgb) -> output 5 x 2400 x 2400
-# rgb = RatioCalculation.add_ratio_bands(rgb)
-# band_ratios = rgb * 25
-
-# generate_json(rgb[3])
+generate_json(rgb[3], date)
